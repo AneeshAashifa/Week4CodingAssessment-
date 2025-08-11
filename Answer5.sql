9fc64f4bc84e1dfc2839bac5c6878d4bfde4f610
AneeshAashifa <aneeshaashifa6@gmail.com>
2025-08-11 17:35:57 +0530

    Added Contents

--- Answer5.sql
+++ Answer5.sql
@@ -1,9 +1,9 @@
 create FUNCTION CountWords(@sentence VARCHAR(MAX))
 RETURNS INT
-AS 
-BEGIN 
-     DECLARE @count INT;
-     SELECT @count= LEN(LTRIM(RTRIM(@sentence)))- LEN(REPLACE(LTRIM(RTRIM(@sentence)), '' , ''))+1;
-     RETURN @count;
-END
+AS
+BEGIN
+    RETURN LEN(@sentence) - LEN(REPLACE(@sentence, ' ', '')) + 1;
+END;
+
+
 
